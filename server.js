0aadff2d21f0a9fef77de507a930b805012b3b2f
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-08-02 14:49:39 +0530

    feat: add lodash zip functions and notes module - Added a new `notes.js` file with a sample function and variable export.

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-function callback(){
-    console.log("add function is successfull adding");
-}
+// import * as fs from 'node:fs';
+// import { userInfo } from 'node:os'
+var _ = require("lodash");
+const notes = require("./notes.js");
 
-var add = function(a, b, callback){
-    var result = a*b;
-    console.log("result", result);
-    callback();
-}
+// const user = userInfo();
 
-add(5,9, callback);
+// fs.appendFile('greeting.txt', 'Hi ' + user.username + '!\n', (err) => {
+//     if (err) throw err;
+//     console.log('File is created');
+// });
+
+// console.log("server file is available");
+
+// var age = notes.age;
+
+// var result = notes.addNumber(age + 18, 10);
+
+// console.log(age);
+// console.log(result);
+
+var data = ["person", "person", 1,2,1,2, 'name', 'age', '2'];
+var filter = _.uniq(data)
+console.log(filter);
+console.log(_.isString(true));
+
+
+
 

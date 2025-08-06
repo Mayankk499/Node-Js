6f6d0363da0beb129a8dfdeca68cc4ff0c8a92e2
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-08-06 16:23:44 +0530

    Create and read data on DB

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const app = express();
+const db = require("./db.js");
+const Person = require("./models/person.js");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
   res.send("hello server");
 });
-app.get("/chicken", function (req, res) {
-  res.send("hello server, i'm here to serve you delicious chicken");
+// POST method to add data in database...
+app.post("/person", async (req, res) => {
+  try {
+    const data = req.body;
+
+    const newPerson = new Person(data);
+
+    const savedPerson = await newPerson.save();
+    console.log("data saved");
+    res.status(200).json(savedPerson);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+// GET method to get data of person from database..
+app.get("/person", async (req, res) => {
+  try {
+    const data = await Person.find();
+    console.log("data fetched");
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({ error: "internal server error" });
+  }
 });
 
-app.get('/idli', (req,  res) => {
-    var customized_idli = {
-        name: 'rava idli',
-        size: '10 cm dia',
-        is_sambhar: true,
-        is_chutney: true
-    }
-    res.send(customized_idli); 
-})
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});

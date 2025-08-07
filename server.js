b9c26a4a921378b85dee1f92ba4048338d638fcd
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-08-07 12:44:27 +0530

    feat: add menu management endpoints and menu model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./db.js");
 const Person = require("./models/person.js");
+const MenuItem = require("./models/menu.js");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
@@ -31,6 +32,47 @@ app.get("/person", async (req, res) => {
     res.status(200).json(data);
   } catch (error) {
     console.log(err);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+app.post("/menu", async (req, res) => {
+  try {
+    const data = req.body;
+
+    const newMenu = new MenuItem(data);
+    const savedmenu = await newMenu.save();
+    console.log("new menu saved");
+    res.status(200).json(savedmenu);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+app.get("/menu", async (req, res) => {
+  try {
+    const data = await MenuItem.find();
+    console.log("menu fetched");
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+
+app.get("/person/:workType", async (req, res) => {
+  try {
+    const workType = req.params.workType;
+    if (workType == "chef" || workType == "manager" || workType == "waiter") {
+      const response = await Person.find({ work: workType });
+      console.log("response fetched");
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({ error: "Invalid work type" });
+    }
+  } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "internal server error" });
   }
 });

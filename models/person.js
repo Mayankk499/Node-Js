5073e5999eb863859c62fbe4f0a1c7f0dc80f1fe
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-08-16 16:10:29 +0530

    feat: enhance person model with username and password fields; integrate passport for authentication

--- models/person.js
+++ models/person.js
@@ -1,35 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-    },
-    work:{
-        type: String,
-        enum: ['chef', 'waiter', 'manager'],
-        required: true
-    },
-    mobile: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    address: {
-        type: String
-    },
-    salary: {
-        type: Number,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  work: {
+    type: String,
+    enum: ["chef", "waiter", "manager"],
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model("Person", personSchema);
 module.exports = Person;
--- server.js
+++ server.js
@@ -1,26 +1,60 @@
 const express = require("express");
 const app = express();
 const db = require("./db.js");
-require('dotenv').config();
-
+require("dotenv").config();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Person = require("./models/person.js");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+
+const logRequest = (req, res, next) => {
+  console.log(
+    `${new Date().toLocaleString()} Request made to : ${req.originalUrl}`
+  );
+  next();
+};
+
+app.get("/", logRequest, function (req, res) {
+  res.send("Welcome to our Hotel Site");
+});
+
+app.use(logRequest);
+
+app.use(
+  new LocalStrategy(async (USERNAME, password, done) => {
+    // Authentication logic goes here
+
+    try {
+      console.log("received credentials:", USERNAME, password);
+      const user = Person.findOne({ username: USERNAME });
+      if (!user) return done(null, false, { message: "Incorrect username." });
+      const isPasswordMatch = user.password === password ? true : false;
+      if (isPasswordMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Incorrect password" });
+      }
+    } catch (error) {}
+  })
+);
 
 app.get("/", function (req, res) {
   res.send("hello server");
 });
 
-const personRoutes = require('./routes/personRoutes.js')
-app.use('/person', personRoutes);
+const personRoutes = require("./routes/personRoutes.js");
+app.use("/person", personRoutes);
 
-const menuRoutes = require('./routes/menuRoutes.js');
+const menuRoutes = require("./routes/menuRoutes.js");
 const router = require("./routes/personRoutes.js");
-app.use('/menu', menuRoutes);
-
-const PORT = process.env.PORT || 3000;
+const Person = require("./models/person.js");
+app.use("/menu", menuRoutes);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
-

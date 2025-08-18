66bbe40229425508c601232d385504dd499b92e6
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-08-18 17:49:27 +0530

    feat: add JWT authentication middleware and token generation; update signup and login routes

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,25 +1,56 @@
 const express = require("express");
 const router = express.Router();
 const Person = require("../models/person");
+const {jwtAuthMiddleware, generateToken} = require('./../jwt');
 
 // POST method to add data in database...
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const data = req.body;
 
     const newPerson = new Person(data);
 
-    const savedPerson = await newPerson.save();
+    const response = await newPerson.save();
     console.log("data saved");
-    res.status(200).json(savedPerson);
+
+    const payload = {
+      id: response.id,
+      username: response.username
+    }
+
+    const token = generateToken(payload);
+
+    res.status(200).json({response: response, token: token});
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "internal server error" });
   }
 });
 
+router.post('/login', async(req, res,) => {
+  try {
+    const {username, password} = req.body;
+
+    const user = await Person.findOne({username: username});
+    if(!user || !(await user.comparePassword(password))){
+      return res.status(401).json({error: 'Invalid username or password'});
+    }
+
+    const payload = {
+      id: user.id,
+      username:  user.username
+    }
+    const token = generateToken(payload);
+
+    res.json({token})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "internal login error" });
+  }
+})
+
 // GET method to get data of person from database..
-router.get("/", async (req, res) => {
+router.get("/",async (req, res) => {
   try {
     const data = await Person.find();
     console.log("data fetched");
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const db = require("./db.js");
 require("dotenv").config({ debug: false });
-const passport = require('./auth.js');
+const passport = require("./auth.js");
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
@@ -12,21 +13,20 @@ const logRequest = (req, res, next) => {
   next();
 };
 
-
 app.use(logRequest);
 
 app.use(passport.initialize());
-const localMiddlewareAuth = passport.authenticate('local', {session: false})
+const localMiddlewareAuth = passport.authenticate("local", { session: false });
 
-app.get("/",function (req, res) {
+app.get("/", function (req, res) {
   res.send("Welcome to our Hotel Site");
 });
 
 const personRoutes = require("./routes/personRoutes.js");
 const menuRoutes = require("./routes/menuRoutes.js");
 
-app.use("/person",localMiddlewareAuth ,  personRoutes);
-app.use("/menu",menuRoutes);
+app.use("/person", personRoutes);
+app.use("/menu", menuRoutes);
 
 const router = require("./routes/personRoutes.js");
 const Person = require("./models/person.js");
